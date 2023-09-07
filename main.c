53765d03f8403cce71d479ff759ba92ae3205c31
mayres-2 <mfss2@cin.ufpe.br>
2023-09-07 13:39:48 -0300

    animações

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 int main(){
 
     InitWindow(0, 0, "As aventuras de Thalya");
-    if (!IsWindowFullscreen()){ ToggleFullscreen(); }
+    //if (!IsWindowFullscreen()){ ToggleFullscreen(); }
     SetTargetFPS(60);
 
     double time = GetTime();
@@ -30,30 +30,67 @@ int main(){
     loadSala5(&(sala[4]), screenWidth, screenHeight);
     loadSala6(&(sala[5]), screenWidth, screenHeight);
 
+
+    // ANIMATION
+        int desenho_capivara = 96;
+        int clique_w = 0, clique_s = 0, clique_a = 0, clique_d = 0;
+    //
+    
     while(!WindowShouldClose()){
         double delta = GetTime() - time;
         time = GetTime();
         capivara.salaAtual = 1;
         int salaAtual = capivara.salaAtual;
 
+
+        
+        
         if (IsKeyDown(KEY_W)) {
             capivara.pos.y -= capivara.speed * delta;
             capivara.hitbox.y = capivara.pos.y;
+            
+            //costas //mudar a forma como anda a cada clique
+            clique_w++; 
+            DrawTextureRec(capivara.sprite, (Rectangle) {(96*6) + (96 * clique_w), 0, 96, 96}, capivara.pos, WHITE);
+            clique_w = clique_w%2;
+            desenho_capivara = (96 * 7);
+
         }
 
         if (IsKeyDown(KEY_S)) {
             capivara.pos.y += capivara.speed * delta;
             capivara.hitbox.y = capivara.pos.y;
+
+            //frente //mudar sprite depois //mudar a forma como anda a cada clique
+            clique_s += 2; 
+            DrawTextureRec(capivara.sprite, (Rectangle) { (96 * clique_s), 0, 96, 96}, capivara.pos, WHITE); 
+            clique_s = clique_s%2;
+            desenho_capivara = (96 * 1);
+
         }
 
         if (IsKeyDown(KEY_D)) {
             capivara.pos.x += capivara.speed * delta;
             capivara.hitbox.x = capivara.pos.x;
+
+            //direita //mudar a forma como anda a cada clique
+            clique_d++;
+            DrawTextureRec(capivara.sprite, (Rectangle) { (96 * clique_d), 0, 96, 96}, capivara.pos, WHITE);
+            clique_d = clique_d%2;
+            desenho_capivara = (96 * 1);
+
         }
 
         if (IsKeyDown(KEY_A)) {
             capivara.pos.x -= capivara.speed * delta;
             capivara.hitbox.x = capivara.pos.x;
+
+            //esquerda //mudar a forma como anda a cada clique
+            clique_a++;
+            DrawTextureRec(capivara.sprite, (Rectangle) {(96*3) + (96 * clique_a), 0, 96, 96}, capivara.pos, WHITE);
+            clique_a = clique_a%2;
+            desenho_capivara = (96 * 4);
+
         }
 
         fixCollision(&capivara, &sala[salaAtual]);
@@ -89,7 +126,10 @@ int main(){
 
         // DEBUG DE COLISÃO--------------------------------------------------------------
 
-        DrawTextureV(capivara.sprite, capivara.pos, RAYWHITE);
+
+        // anima a capivara normalmente desde o último movimento
+        int par = ((int)GetTime())%2;
+        DrawTextureRec(capivara.sprite, (Rectangle) {(desenho_capivara - 96) + (96 * par), 0, 96, 96}, capivara.pos, WHITE);
         capivara.prevPos = capivara.pos;
 
         EndDrawing();
